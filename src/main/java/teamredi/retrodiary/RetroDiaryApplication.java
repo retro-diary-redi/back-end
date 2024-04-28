03d7bf37a14e0c3466e457dcabff77656c81c128
runtime-zer0 <bigstring.zero@gmail.com>
2024-04-28 17:53:37 +0900

    init: 프로젝트 초기 설정 파일

---
+++ src/main/java/teamredi/retrodiary/RetroDiaryApplication.java
@@ -0,0 +1,13 @@
+package teamredi.retrodiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetroDiaryApplication.class, args);
+    }
+
+}
