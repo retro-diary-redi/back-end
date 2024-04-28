02a62a013cf6ebda84662779fe5246cba1261e67
runtime-zer0 <bigstring.zero@gmail.com>
2024-04-28 20:19:11 +0900

    feat : JPA Auditing 기능 추가

--- src/main/java/teamredi/retrodiary/RetroDiaryApplication.java
+++ src/main/java/teamredi/retrodiary/RetroDiaryApplication.java
@@ -2,8 +2,10 @@ package teamredi.retrodiary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RetroDiaryApplication {
 
     public static void main(String[] args) {
