e50f52aa7d2d3d253b263f6971831e25d538ddc1
runtime-zer0 <bigstring.zero@gmail.com>
2024-04-29 02:14:02 +0900

    feat: 다이어리 홈 화면 요청 기능 구현

--- src/main/java/teamredi/retrodiary/repository/diary/DiaryRepositoryImpl.java
+++ src/main/java/teamredi/retrodiary/repository/diary/DiaryRepositoryImpl.java
@@ -1,14 +1,20 @@
 package teamredi.retrodiary.repository.diary;
 
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.Wildcard;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import teamredi.retrodiary.dto.DiaryResponseDTO;
 import teamredi.retrodiary.entity.Diary;
 
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static teamredi.retrodiary.entity.QDiary.*;
 import static teamredi.retrodiary.entity.QMember.*;
@@ -19,22 +25,6 @@ import static teamredi.retrodiary.entity.QMember.*;
 public class DiaryRepositoryImpl implements DiaryRepositoryCustom {
 
     private final JPAQueryFactory jpaQueryFactory;
-
-    @Override
-    public Optional<DiaryResponseDTO> findOneById(Long id) {
-
-        return Optional.ofNullable(jpaQueryFactory
-                .select(Projections.fields(DiaryResponseDTO.class,
-                        diary.title,
-                        diary.mood,
-                        diary.weather,
-                        diary.content,
-                        member.nickname
-                ))
-                .from(diary)
-                .leftJoin(diary.member, member)
-                .fetchOne());
-    }
 
     @Override
     public Optional<DiaryResponseDTO> findDiaryByDateAndUsername(LocalDate date, String username) {
@@ -53,6 +43,21 @@ public class DiaryRepositoryImpl implements DiaryRepositoryCustom {
                         .and(member.username.eq(username))
                 )
                 .fetchOne());
+    }
+
+    @Override
+    public List<String> getEachUserDiaryDateByUsername(String username) {
+        List<LocalDate> contents = jpaQueryFactory
+                .select(diary.date)
+                .from(diary)
+                .leftJoin(diary.member, member)
+                .where(member.username.eq(username))
+                .orderBy(diary.id.asc())
+                .fetch();
+
+        return contents.stream()
+                        .map(LocalDate::toString)
+                        .collect(Collectors.toList());
     }
 
 
